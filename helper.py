2820d8fab302831400e38648f2269c7894b8339f
jonathantsang <j26tsang@gmail.com>
2017-03-25 18:32:11 -0400

    change

--- helper.py
+++ helper.py
@@ -28,24 +28,18 @@ def create_image(rgbArray):
 	import PIL.Image
 	import math
 	
-## Gets all files where the py file is
-def scaleImage():
-	## Scale images to 512x512
-	size = (512,512)
-	## file is a string for the filename\
+def scaleImage(filename):
+	## file is a string for the filename
 	i = 0
 	factor = 1
-	for file in os.listdir("."):
-		factor = 1
-		if file.endswith(".jpg"):
-			img = PIL.Image.open(file)
-			width, height = img.size
-			smallest = min(width,height)
-			factor = math.ceil(512 / smallest)
-			## Adjust by the factor to keep aspect ratio
-			width *= factor
-			height *= factor
-			img = img.resize((width, height))
-			img = img.crop((0, 0, 512, 512))
-			img.save("test_" + str(i) + ".png")
-			i += 1
+	img = PIL.Image.open(filename)
+	width, height = img.size
+	smallest = min(width,height)
+	factor = int(math.ceil(float(512) / float(smallest)))
+	## Adjust by the factor to keep aspect ratio
+	width *= factor
+	height *= factor
+	img = img.resize((width, height))
+	img = img.crop((0, 0, 512, 512))
+	img.save("test_" + str(i) + ".png")
+	i += 1
