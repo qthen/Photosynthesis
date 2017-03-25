0b1c3250c503dc466e1dfef9284e9f1c54767b03
kuroware <philiptsang018@gmail.com>
2017-03-25 10:00:32 -0400

    REsolve

--- helper.py
+++ helper.py
@@ -2,12 +2,17 @@
 
 
 char_to_ix = {}
-for i in xrange(0, 26):
+for i in xrange(1, 27):
 	char_to_ix[chr(ord('a') + i)] = i
-for i in xrange(0, 26):
+for i in xrange(27, 53):
 	char_to_ix[chr(ord('A') + i)] = i + 26
-char_to_ix[' '] = 52
-char_to_ix['.'] = 53
+char_to_ix[' '] = 53
+char_to_ix['.'] = 54
+char_to_ix[':'] = 55
+char_to_ix[','] = 56
+char_to_ix['?'] = 57
+char_to_ix['"'] = 58
+char_to_ix['\''] = 59
 
 def char_to_index(character):
 	return char_to_ix[character]
