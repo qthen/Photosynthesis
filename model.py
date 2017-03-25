9006d272a4c490277b3044c47a6c466bf587fc50
kuroware <philiptsang018@gmail.com>
2017-03-25 02:56:36 -0400

    Model definition

---
+++ model.py
@@ -0,0 +1,14 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+
+class Model(object):
+	def __init__(self, DIMENSIONS = []):
+		self.DIMENSIONS = DIMENSIONS #Set instance constants
+		self.model = Sequential() #Create the model
+
+		#Create the model layers
+		self.model.add
+
+	def train(test_data = None):
+		pass
