08cb797675d73d5ffc89089bf7382696fd3b9af4
kuroware <philiptsang018@gmail.com>
2017-03-25 18:30:59 -0400

    A NN

--- helper.py
+++ helper.py
@@ -1,9 +1,11 @@
+import numpy as np
+from PIL import Image
 #Helper functions - may become outdated as the model grows
 
 char_to_ix = {}
-for i in xrange(1, 27):
-	char_to_ix[chr(ord('a') + i)] = i
-for i in xrange(27, 53):
+for i in range(0, 26):
+	char_to_ix[chr(ord('a') + i)] = i + 1
+for i in range(0, 26):
 	char_to_ix[chr(ord('A') + i)] = i + 26
 char_to_ix[' '] = 53
 char_to_ix['.'] = 54
@@ -11,15 +13,15 @@ char_to_ix[':'] = 55
 char_to_ix[','] = 56
 char_to_ix['?'] = 57
 char_to_ix['"'] = 58
-char_to_ix['\''] = 59
+char_to_ix['\n'] = 59
 
+print(char_to_ix['R'])
 def char_to_index(character):
 	return char_to_ix[character]
 
 def image_array(filename):
 	img = Image.open(filename)
-	arr = np.array(img) # 640x480x4 array
-	arr[20, 30] # 4-vector, just like above
+	arr = np.array(img)
 	return arr
 
 def create_image(rgbArray):
--- model.py
+++ model.py
@@ -1,39 +1,92 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM, Conv2DTranspose
-from helper import char_to_index, 
+from keras.layers import Dense, Dropout, LSTM, Conv2DTranspose, Reshape
+from helper import char_to_index, create_image, image_array
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+from keras.optimizers import Adagrad, Adam
 
 #Constants
-LSTM_DIMENSION = 624
-VOCAB_SIZE = 59
-DENSE_DIMENSION_1 = 16384
+LSTM_DIMENSION = 1024
+VOCAB_SIZE = 60
+DENSE_DIMENSION_1 = 12288
 DENSE_DIMENSION_2 = 512
 DENSE_DIMENSION_3 = 512
 DENSE_DIMENSION_4 = 512
+MAX_LENGTH = 42
 
 class Model(object):
-	def __init__(self, N):
-		self.DIMENSIONS = DIMENSIONS #Set instance constants
+	def __init__(self, N, LR = 0.01, optimizer = 'ada', momentum = 0.0):
 		self.model = Sequential() #Create the model
 
 		self.N = N #The test data size
+		self.LR = LR
+
+		#Create the optimizer
+		if (optimizer == 'ada'):
+			self.optimizer = Adagrad(lr=LR, epsilon=1e-08, decay=0.0)
+		elif (optimizer == 'adam'):
+			self.optimizer = Adam(lr=LR, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+		elif (optimizer == 'sgd'):
+			self.optimizer = SGD(lr=LR, momentum=momentum, decay=0.0, nesterov=False)
+		else:
+			self.optimizer = RMSprop(lr=LR, rho=0.9, epsilon=1e-08, decay=0.0)
+
 
 		#Create the model layers
-		self.model.add(LSTM(LSTM_DIMENSION, input_dim=VOCAB_SIZE, kernel_initializer='random_uniform', activation='tanh'))
+		self.model.add(LSTM(LSTM_DIMENSION, input_shape=(42,VOCAB_SIZE), kernel_initializer='random_uniform', activation='tanh'))
 		self.model.add(Dense(DENSE_DIMENSION_1, activation='relu', kernel_initializer='random_uniform', use_bias=True))
-		self.mode.add(Dropout(0.2))
-		self.model.add(Reshape(4, 4, 1024)) #This needs to be aligned with the actual images
+		self.model.add(Dropout(0.2))
+		self.model.add(Reshape((4, 4, 768))) #This needs to be aligned with the actual images
 
-		#Upsampling
-		self.model.add(Conv2DTranspose(strides = (2, 2), kernel_size = (5, 5), data_format="channels_last")) # 8 x 8
-		self.model.add(Conv2DTranspose(strides = (2, 2), kernel_size = (5, 5), data_format="channels_last")) # 16 x 16
-		self.model.add(Conv2DTranspose(strides = (2, 2), kernel_size = (5, 5), data_format="channels_last")) # 32 x 32
-		self.model.add(Conv2DTranspose(strides = (2, 2), kernel_size = (5, 5), data_format="channels_last")) # 64 x 64
-		self.model.add(Conv2DTranspose(strides = (2, 2), kernel_size = (5, 5), data_format="channels_last")) # 128 x 128
-		self.model.add(Conv2DTranspose(strides = (2, 2), kernel_size = (5, 5), data_format="channels_last")) # 256 x 256
-		self.model.add(Conv2DTranspose(strides = (2, 2), kernel_size = (5, 5), data_format="channels_last")) # 512 x 512
+		# #Upsampling
+		self.model.add(Conv2DTranspose(kernel_initializer = 'random_uniform', filters = 264, strides = (2, 2), kernel_size = (2, 2), data_format="channels_last", activation='relu')) # 8 x 8
+		self.model.add(Conv2DTranspose(kernel_initializer = 'random_uniform', filters = 128 ,strides = (2, 2), kernel_size = (2, 2), data_format="channels_last", activation='relu')) # 16 x 16
+		self.model.add(Conv2DTranspose(kernel_initializer = 'random_uniform', filters = 64, strides = (2, 2), kernel_size = (2, 2), data_format="channels_last", activation='relu')) # 32 x 32
+		self.model.add(Conv2DTranspose(kernel_initializer = 'random_uniform', filters = 32,strides = (2, 2), kernel_size = (2, 2), data_format="channels_last", activation='relu')) # 64 x 64
+		self.model.add(Conv2DTranspose(kernel_initializer = 'random_uniform', filters = 8,strides = (2, 2), kernel_size = (2, 2), data_format="channels_last", activation='relu')) # 128 x 128
+		self.model.add(Conv2DTranspose(kernel_initializer = 'random_uniform', filters = 8,strides = (2, 2), kernel_size = (2, 2), data_format="channels_last", activation='relu')) # 256 x 256
+		self.model.add(Conv2DTranspose(kernel_initializer = 'random_uniform', filters = 3,strides = (2, 2), kernel_size = (2, 2), data_format="channels_last", activation='relu')) # 512 x 512
+
+		#Compiling
+		self.model.compile(optimizer=self.optimizer, loss = 'mean_squared_error')
+
+		#Model summary
+		self.model.summary()
 
 
-	def train(test_data = None):
-		#Prepare the test data
-		for i in xrange(0, self.N):
+	def train(self, test_data = None, epochs = 30):
+
+		captions = []
+		with open("dataset_captions/captions.txt") as fp:
+			for line in fp:
+				caption_text = [char_to_index(character) for character in line] #Convert to indices
+				#Convert to encoding
+				caption_text = to_categorical(caption_text, num_classes = VOCAB_SIZE)
+				captions.append(caption_text)
+		images = []
+		#Prepare the test data and train on batch size = 1
+		for i in range(0, self.N):
+			rgb_img = image_array("dataset/test_{}.jpg".format(i))
+			images.append(rgb_img)
+
+		images = np.array(images)
+		captions = np.array(captions)
+		captions = pad_sequences(captions, maxlen=42)
+
+		#Train here
+		self.model.fit(captions, images, epochs = epochs, batch_size = 4)
+
+		test_data = to_categorical([char_to_index(character) for character in "Red lights in a faraway city."], num_classes = VOCAB_SIZE)
+		test_data = np.array([test_data])
+		test_data = pad_sequences(test_data, maxlen = 42)
+
+		#Predict a result
+		prediction = self.model.predict(test_data, batch_size = 1)
+
+		create_image(prediction[0])
+
+
+
+
+
